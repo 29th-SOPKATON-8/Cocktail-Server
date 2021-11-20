7b925501fe1ec39173cc65fafc6968cfcecdb3a1
LeeJejune <leejejune0625@gmail.com>
2021-11-21 03:33:52 +0900

    [feat] trashPOST
    [feat] trashPOST

--- functions/api/routes/index.js
+++ functions/api/routes/index.js
@@ -1,4 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+router.use('/lover', require('./lover'));
+router.use('/post', require('./post'));
+router.use('/suggest', require('./suggest'));
+
 module.exports = router;
